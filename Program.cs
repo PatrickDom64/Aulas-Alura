cbb666c6036afed4addecdf7f300ad09793997ac
Patrick Dom <44453042+PatrickDom64@users.noreply.github.com>
2022-01-31 22:16:20 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using D1_ByteBank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main()
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();  
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 001;
+            contaDaGabriela.saldo = 1000;
+            
+            Console.WriteLine("Titular:"+contaDaGabriela.titular +"\n");
+        }
+    }
+}
